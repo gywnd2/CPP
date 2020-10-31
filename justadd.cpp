d39944c28ea3e61097667a2ad53b9c5b23fa30c9
gywnd2 <appl4368@gmail.com>
2020-10-31 16:43:05 +0900

    Let me just add two numbers!

---
+++ justadd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	int numa;
+	int numb;
+	int result;
+	std::cout<<"숫자하나 입력 : ";
+	std::cin>>numa;
+	std::cout<<"숫자둘 입력 : ";
+	std::cin>>numb;
+	result=numa+numb;
+	std::cout<<"결과는 ~ !! : ";
+	std::cout<<result<<std::endl;
+	return 0; 
+	
+}
