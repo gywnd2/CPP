7387438e131904a887e4415664fdfbd6ba97da84
gywnd2 <appl4368@gmail.com>
2020-12-04 18:45:08 +0900

    Q02-1

---
+++ Q02/Q02-1/Q02-1-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void SwapPointer(int *ptr1, int *ptr2){
+	int temp;
+	temp=*ptr1;
+	*ptr1=*ptr2;
+	*ptr2=temp;
+}
+
+int main(){
+	int num1=5;
+	int *ptr1=&num1;
+	int num2=10;
+	int *ptr2=&num2;
+	
+	std::cout<<*ptr1<<std::endl;
+	std::cout<<*ptr2<<std::endl;
+	
+	SwapPointer(ptr1, ptr2);
+	
+	std::cout<<"\n"<<*ptr1<<std::endl;
+	std::cout<<*ptr2<<std::endl;
+}
