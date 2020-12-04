5f6fe3708760f93b7997185c974189a69d68d307
gywnd2 <appl4368@gmail.com>
2020-12-04 18:37:12 +0900

    Q02-1-1

---
+++ Q02/Q02-1/Q02-1-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void increase(int &ref)
+{
+	ref++;
+}
+
+void inverse(int *ptr)
+{
+	*ptr=*ptr*(-1);
+}
+
+int main(){
+	int a=0;
+	int *ref=&a;
+	
+	std::cout<<a<<std::endl;
+	
+	increase(a);
+	std::cout<<a<<std::endl;
+	inverse(ref);
+	std::cout<<a<<std::endl;
+	
+	return 0;
+}
+
